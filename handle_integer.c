8058e656456384abd43c2e0c1eccf207d2f725fe
Jack MacDonald <9902@holberstonstudents.com>
2024-11-26 05:16:13 +0000

    Handle integer and print number

---
+++ handle_integer.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * handle_integer - Handles 'i' and 'd' specifiers.
+ * @args: Variadic argument list
+ *
+ * Return: Number of characters printed
+ */
+
+int handle_integer(va_list args)
+{
+	int num = va_arg(args, int);
+	return (print_number(num));
+}

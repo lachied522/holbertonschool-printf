a9be0e145080e898ae14bc3eba32f9dbf9327a2c
root <lachie.duncan12@gmail.com>
2024-11-26 23:02:35 +0000

    Fixed logic for null string

--- handle_string.c
+++ handle_string.c
@@ -3,6 +3,26 @@
 #include <stdlib.h>
 
 /**
+ * print_string - prints string to stdout
+ * @s: string to print
+ * Return: number of chars printed
+ */
+int print_string(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		_putchar(s[i]);
+		i++;
+	}
+
+	return (i);
+}
+
+
+
+/**
  * handle_string- prints string from va_list
  * @args: va_list
  * Return: number of chars printed
@@ -10,19 +30,9 @@
 int handle_string(va_list args)
 {
 	char *s = va_arg(args, char *);
-	int i;
-	int n;
 
 	if (s == NULL)
-		return (handle_string("(null)"));
+		return (print_string("(null)"));
 
-	i = 0;
-	while (s[i] != '\0')
-	{
-		_putchar(s[i]);
-		i++;
-		n++;
-	}
-
-	return (n);
+	return (print_string(s));
 }
